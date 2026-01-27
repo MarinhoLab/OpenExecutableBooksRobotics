e1ca7ab1b84cbc4d54a7b8e8401657813809b458
Murilo Marinho <murilo.marinho@manchester.ac.uk>
2026-01-27 16:48:29 +0000

    [devcontainer] Setup seems to be working, site not showing for some reason on host yet.

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-export DEBIAN_FRONTEND=noninteractive
+dpkg-reconfigure debconf --frontend=noninteractive
 
 apt-get update
 apt-get install -y sudo
-sudo apt-get install -y git python3 python3-pip curl
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y git python3 python3-pip curl
 
 # This is expected to be run at the root of the repository
 pwd
@@ -13,7 +13,8 @@ export PIP_BREAK_SYSTEM_PACKAGES=1
 python3 -m pip install jupyter-book --pre
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
-export NVM_DIR="/.jbdevcontainer/config/nvm"
+NVM_DIR="/.jbdevcontainer/config/nvm"
+export NVM_DIR="$NVM_DIR"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 nvm install 22.17
 
